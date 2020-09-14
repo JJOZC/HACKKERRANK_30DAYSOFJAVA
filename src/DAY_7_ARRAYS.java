ed716af6004f18d8b74ec504abd8e3011575b839
Cagri Ozcevahir <cozcevahir@gmail.com>
2020-09-14 11:51:08 +0300

    DAY7 ADDED

---
+++ src/DAY_7_ARRAYS.java
@@ -0,0 +1,22 @@
+public class DAY_7_ARRAYS {
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        for (int i = n-1; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
